c8bbb236cfff68ef317331e9719f0a32023e5c39
Siebe Paesschesoone <siebe.paesschesoone@telenet.be>
2017-11-23 08:44:13 +0100

    Initial commit

---
+++ ClassLibraryForSiebePaesschesoone/ClassLibraryForSiebePaesschesoone/ClientViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClassLibraryForSiebePaesschesoone
+{
+    public class ClientViewModel
+    {
+        [Required]
+        public string SerialKey { get; set; }
+
+        [Required, DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [Required, DataType(DataType.Text)]
+        public string FamilyName { get; set; }
+
+        [Required, DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [Required, DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Entered phone format is not valid.")]
+        public string PhoneNumber { get; set; }
+
+        [Required, DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+		public string DateOfBirth { get; set; }
+    }
+}
---
+++ ClassLibraryForSiebePaesschesoone/ClassLibraryForSiebePaesschesoone/ValidationViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClassLibraryForSiebePaesschesoone
+{
+    public class ValidationViewModel
+    {
+        [DataType(DataType.Password), Display(Name = "This action requires authentication. Give password: ")]
+        public string password { get; set; }
+    }
+}
